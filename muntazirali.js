2ab921cb2ccde27248582c870988e402869fb0fa
Muntazir Ali <muntazirali2018@gmail.com>
2025-06-20 15:10:15 +0530

    main

---
+++ muntazirali.js
@@ -0,0 +1,244 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'violet') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'lightblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'wheat') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'rebeccapurple') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'tan') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'teal') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'thistle') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'tomato') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'turquoise') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellowgreen') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'indianred') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'indigo') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'ivory') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'oldlace') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'olive') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'olivedrab') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'orange') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'orangered') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'orchid') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'aliceblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'antiquewhite') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'aqua') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'aquamarine') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'azure') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'saddlebrown') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'salmon') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'sandybrown') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'seagreen') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'seashell') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'sienna') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'silver') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'skyblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'slateblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'slategrey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkcyan') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkgoldenrod') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkgray') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkgreen') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkkhaki') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkmagenta') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkolivegreen') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkorange') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkorchid') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'darkred') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'firebrick') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'forestgreen') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'fuchsia') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'gold') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'goldenrod') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'greenyellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'green') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'hotpink') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'khaki') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'lawngreen') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'cadetblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'chocolate') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'coral') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'cornflowerblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'crimson') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'navajowhite') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'navy') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'black') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'brown') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'burlywood') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'magenta') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'maroon') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'midnightblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'mistyrose') {
+      body.style.backgroundColor = e.target.id;
+    }
+
+    if (e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'cyan') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'aquamarine') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'bisque') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+
+  });
+});
+
+
